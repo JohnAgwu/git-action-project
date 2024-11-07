95b152bc3e87a0169fb4a93681d6527cfe504230
John Agwu <agujohnifeanyi69@gmail.com>
2024-11-07 09:07:06 +0000

    commit

---
+++ main.tf
@@ -0,0 +1,101 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+resource "aws_instance" "ec-create" {
+  ami           = "ami-0b4c7755cdf0d9219"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  key_name      = "jonag"
+
+  tags = {
+    Name = "Node2-Created-By-GitAction"
+  }
+
+}
+
+terraform {
+  backend "s3" {
+    bucket = "jonag-terraform-store"
+    key    = "project2/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "192.168.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "192.168.1.0/24"
+  availability_zone       = "eu-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+# Created an Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "project-igw"
+  }
+}
+
+
+# Route Table for the Public Subnet
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Associate the Route Table with the Public Subnet
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_security_group" "frontend_sg" {
+  name   = "frontend-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
